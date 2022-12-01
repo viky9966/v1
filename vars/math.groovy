1ada5a089b768dd32012366a1f9b593bcd473343
viky9966 <118269802+viky9966@users.noreply.github.com>
2022-12-01 18:00:55 +0530

    Create math.groovy

---
+++ vars/math.groovy
@@ -0,0 +1,16 @@
+def sum(a,b)
+{
+ echo "The sum of ${a} and ${b} is ${a+b}"
+ }
+ def sub(a,b)
+{
+ echo "The difference of ${a} and ${b} is ${a-b}"
+ }
+ def mul(a,b)
+{
+ echo "The product of ${a} and ${b} is ${a*b}"
+ }
+ def div(a,b)
+{
+ echo "The sum of ${a} and ${b} is ${a+b}"
+ }

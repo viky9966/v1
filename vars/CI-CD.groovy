a9ae031992754d740e7f8c7dc7f34555a770c5cb
viky9966 <118269802+viky9966@users.noreply.github.com>
2022-12-07 14:18:28 +0530

    Update CI-CD.groovy

--- vars/CI-CD.groovy
+++ vars/CI-CD.groovy
@@ -1,6 +1,6 @@
 def new(a)
 {
-git "https://github.com/viky9966/${a}"
+git 'https://github.com/viky9966/${a}'
 }
 
 def build()
@@ -10,10 +10,10 @@ sh 'mvn package'
 
 def deploy(filename,ip,appname)
 {
-sh "scp /var/lib/jenkins/workspace/${filename}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war"
+sh 'scp /var/lib/jenkins/workspace/${filename}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat9/webapps/${appname}.war'
 }
 
 def selenium(filename)
 {
-sh "java -jar /var/lib/jenkins/workspace/${filename}/testing.jar"
+sh 'java -jar /var/lib/jenkins/workspace/${filename}/testing.jar'
 }
